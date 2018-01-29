19259e89f0bbd6b4f0e606cc7ee7bc1196ba3e9e
jjohanj <34948414+jjohanj@users.noreply.github.com>
2018-01-29 16:03:52 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,25 @@
+<?php
+
+$title = $_GET['titel'];
+$body = $_GET['body'];
+
+try {
+    $conn = new PDO("mysql:host=127.0.0.1;dbname=blog", "root", "");
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO artikelen (titel, body)
+    VALUES ('$title', '$body')";
+    $conn->exec($sql);
+    $last_id = $conn->lastInsertId();
+    echo "Connected successfully</br>";
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+    }
+
+
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+?>
